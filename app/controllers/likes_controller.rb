889e1bd690f48cd531399c00536ea1ffdcfd23a1
daniel <borrerodaniel89@gmail.com>
2020-05-15 20:00:01 -0400

    add style-bootstra, edit: controller like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,9 +4,9 @@ class LikesController < ApplicationController
   def create
     @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
     if @like && @like.active
-      @like.update_atributte(active: false)
+      @like.update(active: false)
     elsif @like && !@like.active
-      @like.update_atributte(active: true)
+      @like.update(active: true)
     else
       @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
     end
